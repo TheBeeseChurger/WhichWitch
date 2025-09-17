e512ae0d64ad1e8db8fdef2520473632e00d6e04
Brandon Underwood <coolbrandon04@gmail.com>
2025-09-16 19:06:00 -0500

    Revert "Pure GDScript notes resource system and moved files"
    This reverts commit edd7af0b69c1dfb3474215cab9f32740c796e3cb.

--- Scripts/SongSystem/DifficultyData.gd
+++
@@ -1,6 +0,0 @@
-# res://scripts/DifficultyData.gd
-class_name DifficultyData
-extends Resource
-
-@export var difficulty_name: String = ""       # e.g., "Easy", "Hard", "Expert"
-@export var notes_file_path: String = ""       # e.g., "res://songs/mysong_hard.dat"
